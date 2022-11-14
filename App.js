68d6a26ddc9e88de574f65d2203ca7604fa0ef83
phạm hồng định <duiga2611@gmail.com>
2022-11-14 15:04:55 +0700

    hoc hanh cai j day

--- App.js
+++ App.js
@@ -1,20 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Text } from 'react-native';
+import { Chat, OverlayProvider, ChannelList,Channel,MessageList,MessageInput} from 'stream-chat-expo';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
-export default function App() {
+import { useChatClient } from './useChatClient';
+import { StreamChat } from 'stream-chat';
+import { chatApiKey, chatUserId } from './chatConfig';
+import { AppProvider } from "./AppContext";
+import {useAppContext} from "./AppContext"
+
+const Stack = createStackNavigator();
+
+const filters = {
+  members: {
+    '$in': [chatUserId]
+  },
+};
+
+const sort = {
+  last_message_at: -1,
+};
+
+const ChannelListScreen = props => {
+  const { setChannel } = useAppContext();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ChannelList
+    onSelect={(channel) => {
+      const { navigation } = props;
+      setChannel(channel);
+      navigation.navigate('ChannelScreen');
+    }}
+      filters={filters}
+      sort={sort}
+    />
+  );
+};
+
+const ChannelScreen = props => {
+  const { channel } = useAppContext();
+  return (
+    <Channel channel={channel}>
+      <MessageList />
+      <MessageInput />
+    </Channel>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const chatClient = StreamChat.getInstance(chatApiKey);
+
+const NavigationStack = () => {
+  const { clientIsReady } = useChatClient();
+
+  if (!clientIsReady) {
+    return <Text>Loading chat ...</Text>
+  }
+  return (
+    <OverlayProvider>
+      <Chat client={chatClient}>
+        <Stack.Navigator>
+          <Stack.Screen name="ChannelList" component={ChannelListScreen} />
+          <Stack.Screen name="ChannelScreen" component={ChannelScreen} />
+        </Stack.Navigator>
+      </Chat>
+    </OverlayProvider>
+  );
+};
+
+export default () => {
+  return (
+    <AppProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+    <SafeAreaView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <NavigationStack />
+      </NavigationContainer>
+    </SafeAreaView>
+    </GestureHandlerRootView>
+    </AppProvider>
+  );
+};
---
+++ chatConfig.js
@@ -0,0 +1,5 @@
+// chatConfig.js
+export const chatApiKey = '3rgpjqmv7w4c';
+export const chatUserId = 'dinh1';
+export const chatUserToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZGluaDEifQ.M4BiCpADQuIkQCJc3r4lSG-c-L5uxBwE-qiWHW1HrnM';
+export const chatUserName = 'dinh1';
